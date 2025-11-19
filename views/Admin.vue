800f7fb2baa9911480072a16be1cb576d6718fcf
鳌拜拜 <sep@aya.yale.edu>
2025-11-19 13:48:45 +0800

    Create Admin.vue

---
+++ views/Admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AdminLogin v-if="!playerStore.isAdmin" />
+    <AdminPanel v-else />
+  </div>
+</template>
+
+<script setup>
+import { usePlayerStore } from '../stores/player'
+import AdminLogin from '../components/AdminLogin.vue'
+import AdminPanel from '../components/AdminPanel.vue'
+
+const playerStore = usePlayerStore()
+</script>
