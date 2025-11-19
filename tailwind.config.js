8f6d7c528520eafca132084aca2fe04b132b7666
鳌拜拜 <sep@aya.yale.edu>
2025-11-19 13:37:28 +0800

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Types.Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
