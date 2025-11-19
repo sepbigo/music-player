f2bd8a56355ab2aba800a8a9e083c8ece1124af5
鳌拜拜 <sep@aya.yale.edu>
2025-11-19 13:44:04 +0800

    Create Home.vue

---
+++ views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <Player />
+    <SongList />
+  </div>
+</template>
+
+<script setup>
+import Player from '../components/Player.vue'
+import SongList from '../components/SongList.vue'
+</script>
