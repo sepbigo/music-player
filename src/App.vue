ec122406c4129228433f7bfb00678eec2e7c9527
鳌拜拜 <sep@aya.yale.edu>
2025-11-19 13:54:30 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+.app {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+}
+</style>
